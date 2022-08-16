9d4a8c827e6f77ad16858416c626d06dedc654ec
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-16 19:53:10 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+  render(<FavoritePokemons />);
+  const pElement = screen.getByText(/No favorite pokemon found/i);
+  expect(pElement).toBeInTheDocument();
+});
