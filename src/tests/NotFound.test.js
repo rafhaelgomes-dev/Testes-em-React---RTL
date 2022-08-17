858ce2dee7f356343fbf3e9015d818ee87c5de32
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-16 22:04:44 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+test('Teste se a página tem um heading h2 com o texto Page requested not found', () => {
+  render(<NotFound />);
+  const h2Element = screen.getByRole('heading', { level: 2, name: /Page/ });
+  expect(h2Element).toBeInTheDocument();
+});
+
+test('Teste se a página mostra a imagem', () => {
+  render(<NotFound />);
+  const elementoImagem = screen.getByAltText(/Pikachu/);
+  expect(elementoImagem).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
